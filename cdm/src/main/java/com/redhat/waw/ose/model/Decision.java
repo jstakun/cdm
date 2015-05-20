f3a64ea4dba11c161d4971145de32df9900b356d
jstakun <jstakun@redhat.com>
2015-05-20 09:03:14 +0200

    Initial commit

---
+++ cdm/src/main/java/com/redhat/waw/ose/model/Decision.java
@@ -0,0 +1,22 @@
+package com.redhat.waw.ose.model;
+
+public class Decision {
+	private String value;
+	private String id;
+	
+	public String getValue() {
+		return value;
+	}
+	
+	public void setValue(String name) {
+		this.value = name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+}
