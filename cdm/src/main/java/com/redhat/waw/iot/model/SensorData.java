73f206083dd00aab3b856737c1c9ea51e0e9126c
jstakun <jstakun@redhat.com>
2016-06-06 14:12:15 +0200

    added sensor data

---
+++ cdm/src/main/java/com/redhat/waw/iot/model/SensorData.java
@@ -0,0 +1,64 @@
+package com.redhat.waw.iot.model;
+
+
+import java.io.Serializable;
+
+public class SensorData implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	private long timestamp;
+	
+	private int a;
+	
+	private float b;
+	
+	private double c;
+	
+	private String sensorId;
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public float getB() {
+		return b;
+	}
+
+	public void setB(float b) {
+		this.b = b;
+	}
+
+	public double getC() {
+		return c;
+	}
+
+	public void setC(double c) {
+		this.c = c;
+	}
+
+	public String getSensorId() {
+		return sensorId;
+	}
+
+	public void setSensorId(String sensorId) {
+		this.sensorId = sensorId;
+	}
+	
+	public String toString() {
+		return sensorId + ": " + timestamp + "," + a +  "," + b + "," + c;
+	}
+
+}
